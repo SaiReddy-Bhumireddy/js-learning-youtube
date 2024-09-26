df612f062ff51e7c351a3265c2bc1c3c563f7def
SaiReddy-Bhumireddy <saireddy256.engg@gmail.com>
2024-09-26 03:10:56 +0000

    Date and Time

---
+++ 01_basics/01_datesInJs.js
@@ -0,0 +1,11 @@
+let todayDate = new Date();
+console.log(todayDate.toString()); //This method gives date in local in which location or country we are
+console.log(todayDate.toLocaleString());// This also gives date of local but in local format
+console.log(todayDate.toDateString());// This gives date without time 
+
+let myTime = Date.now();
+console.log(myTime);
+
+
+
+
