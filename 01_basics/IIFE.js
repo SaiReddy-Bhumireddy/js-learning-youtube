4e35baeb85b2db47e5e273680d2ef0f431d03ce4
SaiReddy-Bhumireddy <saireddy256.engg@gmail.com>
2024-09-27 17:18:58 +0000

    IIFE in javascript

---
+++ 01_basics/IIFE.js
@@ -0,0 +1,18 @@
+// Immediately Invoked Function Expression IIFE
+
+(function chai(){
+    //This is Named IIFE
+    console.log("Learned about IIFE in JS");
+    
+})();// semicolon is must if there are two IIFE function need to write
+// Semicolon separates each IIFE and executes them one by one
+
+// While writing IIFE there are two (), () parenthesis
+// one () wrapping up the function definition
+//Other () is to execution of wrapped function
+
+(() => {
+    console.log("Getting it clearly");
+    
+})()
+
