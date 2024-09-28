361749d024d47f6a1b4d89e9322c7ed42f8db623
SaiReddy-Bhumireddy <saireddy256.engg@gmail.com>
2024-09-28 13:33:16 +0000

    While and Do while in JS

--- 01_basics/whileAndDo.js
+++ 01_basics/whileAndDo.js
@@ -8,3 +8,11 @@ while (num<=10) {
     
     num++;
 }
+// +++++++++ Do while loop ++++++++++++
+// when the condition is false and want to print the value atleast once dowhile loop is used
+
+let score = 5;
+do {
+    console.log(`The runs are ${score}`);
+    
+} while (score>5);// In this the condition is false at first, but it will executes ONCE
